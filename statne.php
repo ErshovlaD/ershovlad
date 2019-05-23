5a13c5bdc6f6c7d198342817ba5179a83c76223d
ErshovlaD <ershovlad@mail.ru>
2019-05-24 01:09:45 +0400

    Add files via upload

---
+++ statne.php
@@ -0,0 +1,22 @@
+﻿<?php
+$access_token = 'ee9268351d0952cfd081fefd28362ada9c4eba1f18d891445fc325b783d901b4deeadd771b04ae1a5666b';//Токен брать тут u.to/token-vk-dlja-avtostatusa/EnTlBQ
+
+#Устанавливаем свою страну, Украина = Europe/Kiev  | Россия = Europe/Moscow
+#Дальше я не знаю:)
+
+$status = "test";
+
+$statusSet = curl('https://api.vk.com/method/status.set?text='.urlencode($status).'&v=3.0&access_token='.$access_token);
+
+#Курлик курлик KO KO KO xD
+function curl( $url ){
+$ch = curl_init( $url );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+$response = curl_exec( $ch );
+curl_close( $ch );
+return $response;
+}
+#Создатель скрипта не я. vk.com/Almazik2015
+?>
